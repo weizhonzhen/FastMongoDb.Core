5a095c74900f8f3f3953e631e39b55794d63bd28
weizhonzhen <czdswzz@126.com>
2021-01-05 17:46:12 +0800

    Add files via upload

---
+++ MongoDbExtension.cs
@@ -0,0 +1,18 @@
+﻿using FastUntility.Core;
+using System;
+using FastMongoDb.Core.Repository;
+using FastMongoDb.Core;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class MongoDbExtension
+    {
+        public static IServiceCollection AddFastMongoDb(this IServiceCollection serviceCollection, Action<ConfigModel> optionsAction = null)
+        {
+            serviceCollection.Configure(optionsAction);
+            serviceCollection.AddSingleton<IMongoDbRepository, MongoDbRepository>();
+            ServiceContext.Init(new ServiceEngine(serviceCollection.BuildServiceProvider()));
+            return serviceCollection;
+        }
+    }
+}
